99dd3afce337807d273c1af1f5b58a013b445836
AndreyMpr <andreympr@gmail.com>
2024-06-04 17:46:52 +0900

    Rewrite getComputerChoice function.

---
+++ rps/janken.js
@@ -0,0 +1,21 @@
+function getComputerChoice(){
+    let computer = Math.floor(Math.random() * 3);
+
+    let computerChoice;
+
+if (computer === 0) {
+    computerChoice = "scissors";
+}
+else if (computer === 1) {
+    computerChoice = "rock";}
+
+else {
+    computerChoice = "paper";
+}
+
+console.log(`Computer chose ${computerChoice}!`);
+
+return computerChoice
+}
+
+getComputerChoice()
