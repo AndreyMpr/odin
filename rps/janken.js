352f390449acbf0c8777835caea50e1c797bcc3d
AndreyMpr <andreympr@gmail.com>
2024-06-05 17:29:12 +0900

    Write getHumanChoice function into the new script.

--- rps/janken.js
+++ rps/janken.js
@@ -15,7 +15,23 @@ function getComputerChoice(){
 
     console.log(`Computer chose ${computerChoice}!`);
 
-    return computerChoice
-}
+    return computerChoice;
+};
 
-getComputerChoice()
+function getHumanChoice(){
+    
+    let humanChoice = prompt(`Chose something, you know that computer has chosen ${computerChoice}, so it has to be en easy choice.`);
+
+    if (humanChoice.toLowerCase() == "rock") {
+        console.log("So you are going with rock");
+    } else if (humanChoice.toLowerCase() == "paper") {
+        console.log("So you are goind with paper");
+    } else if (humanChoice.toLowerCase() == "scissors") {
+        console.log("So you are going with scissors");
+    } else {
+        console.log('Please play along and type in one of the following: "Rock" or "Paper" or "Scissors"');
+        setTimeout(() => {getHumanChoice()}, 2000);
+    }
+    return humanChoice;
+};
+
