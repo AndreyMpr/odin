1514c916218058108c49a99de3eb488018cbcc54
AndreyMpr <andreympr@gmail.com>
2024-06-07 22:01:25 +0900

    Move other functions inside playRound function declaration.

--- rps/janken.js
+++ rps/janken.js
@@ -1,44 +1,10 @@
-function getComputerChoice(){
-    let computer = Math.floor(Math.random() * 3);
-
-    let computerChoice;
-
-    if (computer === 0) {
-        computerChoice = "scissors";
-    }
-    else if (computer === 1) {
-        computerChoice = "rock";}
-
-    else {
-        computerChoice = "paper";
-    }
-
-    console.log(`Computer chose ${computerChoice}!`);
-
-    return computerChoice;
-};
 
 
 
-function getHumanChoice(){
-    
-    let humanChoice = prompt(`Chose something, you know that computer has chosen ${computerSelection}, so it has to be en easy choice.`);
 
-    if (humanChoice.toLowerCase() == "rock") {
-        console.log("So you are going with rock");
-    } else if (humanChoice.toLowerCase() == "paper") {
-        console.log("So you are goind with paper");
-    } else if (humanChoice.toLowerCase() == "scissors") {
-        console.log("So you are going with scissors");
-    } else {
-        console.log('Please play along and type in one of the following: "Rock" or "Paper" or "Scissors"');
-        setTimeout(() => {getHumanChoice()}, 2000);
-    }
-    return humanChoice;
-};
 
-let computerSelection = getComputerChoice();
-let humanSelection = getHumanChoice();
+
+
 
 
 
@@ -50,6 +16,46 @@ function playGame() {
 
 
     function playRound() {
+
+        function getComputerChoice(){
+            let computer = Math.floor(Math.random() * 3);
+        
+            let computerChoice;
+        
+            if (computer === 0) {
+                computerChoice = "scissors";
+            }
+            else if (computer === 1) {
+                computerChoice = "rock";}
+        
+            else {
+                computerChoice = "paper";
+            }
+        
+            console.log(`Computer chose ${computerChoice}!`);
+        
+            return computerChoice;
+        };
+
+        function getHumanChoice(){
+    
+            let humanChoice = prompt(`Chose something, you know that computer has chosen ${computerSelection}, so it has to be en easy choice.`);
+        
+            if (humanChoice.toLowerCase() == "rock") {
+                console.log("So you are going with rock");
+            } else if (humanChoice.toLowerCase() == "paper") {
+                console.log("So you are goind with paper");
+            } else if (humanChoice.toLowerCase() == "scissors") {
+                console.log("So you are going with scissors");
+            } else {
+                console.log('Please play along and type in one of the following: "Rock" or "Paper" or "Scissors"');
+                setTimeout(() => {getHumanChoice()}, 2000);
+            }
+            return humanChoice;
+        };
+
+        let computerSelection = getComputerChoice();
+        let humanSelection = getHumanChoice();
 
         getComputerChoice();
         getHumanChoice();
