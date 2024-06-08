5ad92075e9923d1c4fbca7d6d920f958c33fec20
AndreyMpr <andreympr@gmail.com>
2024-06-08 21:28:16 +0900

    Leave console.log at every stage to see how the script works.

--- rps/janken.js
+++ rps/janken.js
@@ -3,9 +3,11 @@ function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
-
+console.log("l1");
 
     function playRound() {
+
+        console.log("l2");
 
         function getComputerChoice(){
             let computer = Math.floor(Math.random() * 3);
@@ -23,11 +25,12 @@ function playGame() {
             }
         
             console.log(`Computer chose ${computerChoice}!`);
-        
+            console.log("l3");
             return computerChoice;
         };
 
         function getHumanChoice(){
+            console.log("l4");
     
             let humanChoice = prompt(`Chose something, you know that computer has chosen ${computerSelection}, so it has to be en easy choice.`);
         
@@ -41,14 +44,22 @@ function playGame() {
                 console.log('Please play along and type in one of the following: "Rock" or "Paper" or "Scissors"');
                 setTimeout(() => {getHumanChoice()}, 2000);
             }
+            console.log("l5");
             return humanChoice;
+            
         };
+
+        console.log("l6");
 
         let computerSelection = getComputerChoice();
         let humanSelection = getHumanChoice();
 
+        console.log("l7");
+
         getComputerChoice();
         getHumanChoice();
+
+        console.log("l8");
 
         if ((computerSelection == "scissors") && (humanSelection == "paper")) {
             console.log("You lose"), computerScore++;
@@ -70,17 +81,26 @@ function playGame() {
 
         } else {console.log("That's a draw!")};
 
+        console.log("l9");
+
     }
 
+    console.log("l10");
+
     playRound();
     playRound();
     playRound();
     playRound();
     playRound();
 
+    console.log("l11");
+
     return humanScore, computerScore;
+    
 
 
 };
+console.log("l12");
 
 console.log(playGame());
+console.log("l3");
