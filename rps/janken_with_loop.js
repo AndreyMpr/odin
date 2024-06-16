b0a69846a82dc963ae42f96ebccc5167e144459d
AndreyMpr <andreympr@gmail.com>
2024-06-16 16:02:56 +0900

    Add getComputerChoice function.

--- rps/janken_with_loop.js
+++ rps/janken_with_loop.js
@@ -1,3 +1,22 @@
+function getComputerChoice() {
+    let computer = Math.floor(Math.random() * 3);
+
+    let computerChoice;
+
+    if (computer === 0) {
+        computerChoice = "scissors";
+    }
+    else if (computer === 1) {
+        computerChoice = "rock";}
+
+    else {
+        computerChoice = "paper";
+    }
+
+    console.log(`Computer chose ${computerChoice}!`);
+    return computerChoice;
+};
+
 function playGame() {
 
     let humanScore = 0;
